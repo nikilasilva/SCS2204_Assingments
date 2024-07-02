de80ba6cdd04d06cd4798ec59fbee1898a34a750
nikilasilva <118242917+nikilasilva@users.noreply.github.com>
2024-07-02 11:11:13 +0530

    week3 final

--- week3/q4.scala
+++ week3/q4.scala
@@ -1,8 +1,10 @@
 object D3Q4 {
-    def main(args: Array[String]): Unit = {        
-        var integers = List(1, 2, 3, 4, 5)
-        printf(f"The list of integers : ${integers}\n")
-        printf(f"The sum of even numbers : ${sum(integers)}")
+    def main(args: Array[String]): Unit = {   
+        printf(f"Enter list of integers :\n")
+        val userInput=scala.io.StdIn.readLine()
+        val nums=userInput.split(" ").map(_.toInt).toList
+
+        printf(f"The sum of even numbers : ${sum(nums)}")
 
         def sum(integers :List[Int]):Int = {
                 integers.filter(_%2==0).sum
